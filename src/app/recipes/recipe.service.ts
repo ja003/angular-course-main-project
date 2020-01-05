74f611c0b94f0efa24c9d9fe4b525385e884b454
Adam Jurík <ja004@seznam.cz>
2020-01-05 18:15:38 +0100

    121. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-      ];
+        new Recipe(
+            'Schnitzel',
+            'tasty schnitzel',
+            'https://i.pinimg.com/originals/30/17/db/3017dbc3c2ee82045641b2a181e75ea8.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient("French fries", 20)
+            ]
+        ),
+        new Recipe(
+            'Burger',
+            'fat burger',
+            'https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient("Meat", 1)
+            ]
+        )
+    ];
 
     getRecipes() {
         return this.recipes.slice();
