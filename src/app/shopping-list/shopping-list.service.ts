cb14ab3d8a1d495374cf24e66e634810a9ed2a63
Adam Jurík <ja004@seznam.cz>
2020-01-19 10:53:22 +0100

    221. Updating existing Items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -30,4 +30,10 @@ export class ShoppingListService {
         this.ingredients.push(...ingredients);//pass ingredients as list
         this.ingredientsChanged.next(this.ingredients.slice());
     }
+    
+    updateIngredient(index:number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
 }
