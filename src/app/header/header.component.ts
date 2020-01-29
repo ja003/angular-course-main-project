08b87c1ff2f17aaf6251c511a5065184d277d5c7
Adam Jurik <adam.jurik@bisimulations.com>
2020-01-29 15:33:21 +0100

    281. Fetching Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) { }
-  
+  constructor(private dataStorageService: DataStorageService) {}
+
   onSaveData() {
     this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes();
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
 
 
 
@@ -20,5 +21,12 @@ export class DataStorageService {
             .subscribe(response => {
                 console.log(response);
             });
+    }
+
+    fetchRecipes() {
+      this.http.get<Recipe[]>('https://ng-course-recipe-book-3edd1.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
     }
 }
