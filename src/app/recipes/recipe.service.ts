baa806be8d978884a7c3bc38d00ea061e0cd8eb4
Adam Jurik <adam.jurik@bisimulations.com>
2020-01-29 15:39:13 +0100

    282. Transforming Response Data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,26 +8,28 @@ import { Subject } from "rxjs";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Schnitzel',
-            'tasty schnitzel',
-            'https://i.pinimg.com/originals/30/17/db/3017dbc3c2ee82045641b2a181e75ea8.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient("French fries", 20)
-            ]
-        ),
-        new Recipe(
-            'Burger',
-            'fat burger',
-            'https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient("Meat", 1)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Schnitzel',
+    //         'tasty schnitzel',
+    //         'https://i.pinimg.com/originals/30/17/db/3017dbc3c2ee82045641b2a181e75ea8.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient("French fries", 20)
+    //         ]
+    //     ),
+    //     new Recipe(
+    //         'Burger',
+    //         'fat burger',
+    //         'https://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient("Meat", 1)
+    //         ]
+    //     )
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) { }
 
